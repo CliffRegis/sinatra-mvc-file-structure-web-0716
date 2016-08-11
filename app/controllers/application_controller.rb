ca8bff1d38c7d3c36bd81453f31cc96d34b8388f
Clifton Regis <github email address>
2016-08-11 14:00:32 -0400

    created dog class with Andrew

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,21 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+
+
+   
+  attr_accessor :name, :breed, :age
+
+  @@all =[]
+ def initialization(name, breed, age)
+ 	@name = name
+ 	@breed = breed
+ 	@age = age
+ 	@@all << self
+
+ end
+
+ def self.all
+    @@all
+ end
 end
